119de734994e1d5d2c5b84c4ccbe47ebb9159c7a
Randy Barlow <randy@electronsweatshop.com>
2019-02-24 21:25:00 -0500

    Use a buffered reader to read the config file.
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 //!
 //! ```rpick``` helps pick items from a list of choices, using a Gaussian distribution.
 use std::collections::BTreeMap;
+use std::error::Error;
 use std::fs::File;
 use std::fs::OpenOptions;
-use std::io::ErrorKind;
 use std::io::{self, BufRead};
 use std::io::prelude::*;
+use std::io::BufReader;
 
 use rand::distributions::{Distribution,Normal};
 use serde::{Serialize, Deserialize};
@@ -51,7 +52,7 @@ struct ConfigCategory {
 
 fn main() {
     let args = Cli::from_args();
-    let mut config = _read_config();
+    let mut config = _read_config().unwrap();
     let stddev = (config[&args.category].choices.len() as f64) / 3.0;
     let normal = Normal::new(0.0, stddev);
     let mut accept = false;
@@ -90,26 +91,13 @@ fn _get_config_file_path() -> String {
 
 
 /// Return the user's config as a BTreeMap.
-fn _read_config() -> BTreeMap<String, ConfigCategory> {
+fn _read_config() -> Result<BTreeMap<String, ConfigCategory>, Box<Error>> {
     let config_file_path = _get_config_file_path();
-    let f = File::open(&config_file_path);
+    let f = File::open(&config_file_path)?;
+    let reader = BufReader::new(f);
 
-    let mut f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => {
-                return BTreeMap::new();
-            },
-            other_error => panic!("There was a problem opening the file: {:?}", other_error),
-        },
-    };
-
-    let mut yaml_string = String::new();
-    let error_msg = format!("Could not read {}", &config_file_path);
-    f.read_to_string(&mut yaml_string).expect(&error_msg);
-    let config: BTreeMap<String, ConfigCategory> = serde_yaml::from_str(
-        &yaml_string).expect("Unable to parse config file.");
-    return config;
+    let config: BTreeMap<String, ConfigCategory> = serde_yaml::from_reader(reader)?;
+    return Ok(config);
 }
 
 
