514cb90c899247403239e412baff23991ba96668
Randy Barlow <randy@electronsweatshop.com>
2020-03-17 23:06:22 -0400

    Request a backtrace when tests fail
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- devel/test.sh
+++ devel/test.sh
@@ -3,5 +3,6 @@
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 PARAMS=$@
 
-podman run --rm -it -v $SRC_DIR:/rpick:z -e RPICK_CONFIG="/rpick/devel/config.yml" rpick:dev \
+podman run --rm -it -v $SRC_DIR:/rpick:z -e RPICK_CONFIG="/rpick/devel/config.yml" \
+	-e RUST_BACKTRACE=1 rpick:dev \
 	bash -c "cd /rpick && cargo test && cargo clippy --all-targets --all-features -- -D warnings && cargo doc"
