6b810d26e9037a541b02a37c650078fb52926160
Randy Barlow <randy@electronsweatshop.com>
2020-05-14 21:51:59 -0400

    Allow cargo.sh to use network
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- devel/cargo.sh
+++ devel/cargo.sh
@@ -3,5 +3,5 @@
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
 PARAMS=$@
 
-podman run --rm -it -v $SRC_DIR:/rpick:z -e RPICK_CONFIG="/rpick/devel/config.yml" rpick:dev \
+podman run --network=host --rm -it -v $SRC_DIR:/rpick:z -e RPICK_CONFIG="/rpick/devel/config.yml" rpick:dev \
 	bash -c "cd /rpick && cargo $PARAMS"
