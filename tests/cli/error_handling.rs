35d280424b5066c7016c4372c3aa67ed8cab3b0a
Randy Barlow <randy@electronsweatshop.com>
2025-02-16 20:41:40 -0500

    clippy: Fix a test docblock
    This was a comment, but it was intended to be a module docblock.
    
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- tests/cli/error_handling.rs
+++ tests/cli/error_handling.rs
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/// The tests in this module assert correct error handling.
+//! The tests in this module assert correct error handling.
 
 const CATEGORY_NOT_FOUND_CONFIG: &str = "
 ---
